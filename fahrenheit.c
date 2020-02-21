87ee3bca87572e8312201f2be238618389e7bada
Mac <storiaca@gmail.com>
2020-02-21 16:25:53 +0100

    Add header to table

--- fahrenheit.c
+++ fahrenheit.c
@@ -14,10 +14,11 @@ int main(void)
     step = 20;   /* step size */
 
     fahr = lower;
+    printf("Fahr Cel\n");
     while (fahr <= upper)
     {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
